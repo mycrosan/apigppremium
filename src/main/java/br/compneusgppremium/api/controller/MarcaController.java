9a2b3431909af63cd1fcbb36157e99017d57197e
Sandy Santos <sandysantos@mycrosan.com.br>
2021-09-01 01:04:34 -0300

    v1.0.4: Ajustes marca

---
+++ src/main/java/br/compneusgppremium/api/controller/MarcaController.java
@@ -0,0 +1,50 @@
+package br.compneusgppremium.api.controller;
+
+import br.compneusgppremium.api.controller.model.MarcaModel;
+import br.compneusgppremium.api.repository.MarcaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MarcaController {
+
+    @Autowired
+    private MarcaRepository repository;
+
+    @GetMapping(path = "/api/marca")
+    public List<MarcaModel> findAll() {
+        var it = repository.findAll();
+        var marcas = new ArrayList<MarcaModel>();
+        it.forEach(e -> marcas.add(e));
+        return marcas;
+    }
+
+    @GetMapping(path = "/api/marca/{id}")
+    public ResponseEntity consultar(@PathVariable("id") Integer id) {
+        return repository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(path = "/api/marca")
+    public Object salvar(@RequestBody MarcaModel marca) {
+        try {
+            return repository.save(marca);
+        } catch (Exception ex) {
+            return ex;
+        }
+    }
+
+    @DeleteMapping(path = "/api/marca/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+}
