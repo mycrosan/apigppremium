4e415e0f6068a004d588e74fe5ec102a4abadcf4
Sandy Santos <sandysantos@mycrosan.com.br>
2021-10-20 00:40:34 -0300

    working - credenciados

--- src/main/java/br/compneusgppremium/api/GpPremiumApplication.java
+++ src/main/java/br/compneusgppremium/api/GpPremiumApplication.java
@@ -1,6 +1,5 @@
 package br.compneusgppremium.api;
 
-import br.compneusgppremium.api.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,14 +10,6 @@ public class GpPremiumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GpPremiumApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
 	}
 
 }
--- src/main/java/br/compneusgppremium/api/controller/FileUploadController.java
+++ src/main/java/br/compneusgppremium/api/controller/FileUploadController.java
@@ -1,66 +1,91 @@
 package br.compneusgppremium.api.controller;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
 
-import br.compneusgppremium.api.storage.StorageFileNotFoundException;
-import br.compneusgppremium.api.storage.StorageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-@Controller
-class FileUploadController {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-    private final StorageService storageService;
+@CrossOrigin
+@RestController
+@RequestMapping("api/upload")
+class ImageController {
+    @PostMapping
+    public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file)
+    {
+        if (file.isEmpty()){
+            throw new RuntimeException("Arquivo não valido!");
+        }
+        String folder = "/Users/sandy/Documents/Projetos/gp-premium";
+        try {
+            Path pathFolder = Paths.get(folder);
+            Files.createDirectories(pathFolder);
 
-    @Autowired
-    public FileUploadController(StorageService storageService) {
-        this.storageService = storageService;
-    }
+            Path pathFile = Paths.get(folder + file.getOriginalFilename());
+            Files.write(pathFile, file.getBytes());
 
-    @GetMapping("/api/upload")
-    public String listUploadedFiles(Model model) throws IOException {
 
-        model.addAttribute("files", storageService.loadAll().map(
-                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                        "serveFile", path.getFileName().toString()).build().toUri().toString())
-                .collect(Collectors.toList()));
-
-        return "uploadForm";
-    }
-
-    @GetMapping("/files/{filename:.+}")
-    @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-
-        Resource file = storageService.loadAsResource(filename);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-    }
-
-    @PostMapping("/api/upload")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
-
-        storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
-        return "redirect:/";
-    }
-
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-        return ResponseEntity.notFound().build();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Tudo ok Picareta Dev");
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
+
+
+
+//@Controller
+//class FileUploadController {
+//
+//    private final StorageService storageService;
+//
+//    @Autowired
+//    public FileUploadController(StorageService storageService) {
+//        this.storageService = storageService;
+//    }
+//
+//    @GetMapping("/api/upload")
+//    public String listUploadedFiles(Model model) throws IOException {
+//
+//        model.addAttribute("files", storageService.loadAll().map(
+//                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+//                        "serveFile", path.getFileName().toString()).build().toUri().toString())
+//                .collect(Collectors.toList()));
+//
+//        return "uploadForm";
+//    }
+//
+//    @GetMapping("/files/{filename:.+}")
+//    @ResponseBody
+//    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+//
+//        Resource file = storageService.loadAsResource(filename);
+//        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+//                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+//    }
+//
+//    @PostMapping("/api/upload")
+//    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+//                                   RedirectAttributes redirectAttributes) {
+//
+//        storageService.store(file);
+//        redirectAttributes.addFlashAttribute("message",
+//                "You successfully uploaded " + file.getOriginalFilename() + "!");
+//
+//        return "redirect:/";
+//    }
+//
+//    @ExceptionHandler(StorageFileNotFoundException.class)
+//    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+//        return ResponseEntity.notFound().build();
+//    }
+//
+//}
