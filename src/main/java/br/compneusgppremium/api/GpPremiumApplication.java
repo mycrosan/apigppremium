4e16d74b2eb390ed0031fdd0a6cd169be972f6f0
Sandy Santos <sandysantos@mycrosan.com.br>
2021-10-23 12:10:01 -0300

    working - renomeando o arquivo do disco

--- src/main/java/br/compneusgppremium/api/GpPremiumApplication.java
+++ src/main/java/br/compneusgppremium/api/GpPremiumApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.annotation.Resource;
-
-
 @SpringBootApplication
 public class GpPremiumApplication {
 
--- src/main/java/br/compneusgppremium/api/controller/FileUploadController.java
+++ src/main/java/br/compneusgppremium/api/controller/FileUploadController.java
@@ -1,18 +1,24 @@
 package br.compneusgppremium.api.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import br.compneusgppremium.api.message.ResponseMessage;
 import br.compneusgppremium.api.service.FilesStorageService;
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@CrossOrigin("http://localhost:8081")
 public class FileUploadController {
 
 	@Autowired
@@ -29,12 +34,44 @@ public class FileUploadController {
 	@PostMapping("/api/upload")
 	public ResponseEntity<ResponseMessage> uploadFiles(@RequestParam("files") MultipartFile[] files) {
 		String message = "";
+
 		try {
 			List<String> fileNames = new ArrayList<>();
 
 			Arrays.asList(files).stream().forEach(file -> {
+
+				String name = file.getOriginalFilename();
+				String fname="", ext="", totalname="";
+				System.out.println("\n\nFile name : " + name + "\n\n");
+				Pattern p = Pattern.compile(".[a-zA-Z0-9]+");//find all matches where first char is '.' then alphanumeric
+				Matcher m1 = p.matcher(name);//it will store all matches but we want last match eg abc.def.jpg we need .jpg ext
+				while (m1.find())
+				{
+					ext = m1.group();
+				}
+
+//				fname = name.substring(0, name.length()-(ext.length()));
+				fname = UUID.randomUUID().toString();
+				System.out.println("Name : " + fname);
+				System.out.println("Ext  : " + ext);
+				totalname = fname;
+				if(ext != "")//if ext is there concat
+					totalname += ext;
+				System.out.println("Full name  : " + totalname);
+
 				storageService.save(file);
-				fileNames.add(file.getOriginalFilename());
+				fileNames.add(totalname);
+				try {
+					String filename = file.getOriginalFilename(); // Give a random filename here.
+					byte[] bytes = file.getBytes();
+					String insPath = "uploads/" + totalname;
+					// Directory path where you want to save ;
+					Files.write(Paths.get(insPath), bytes);
+				}
+				catch (IOException e) {
+					System.out.println(e);
+				}
+
 			});
 
 			message = "Uploaded the files successfully: " + fileNames;
@@ -44,6 +81,8 @@ public class FileUploadController {
 			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
 		}
 	}
+
+
 
 //	@GetMapping("/files")
 //	public ResponseEntity<List<FileInfo>> getListFiles() {
