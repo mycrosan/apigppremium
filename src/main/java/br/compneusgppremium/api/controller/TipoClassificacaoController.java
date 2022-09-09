7b8d9f630a5de9537365e4c4517b9d3cc2e0d769
Sandy Santos <sandysantos@mycrosan.com.br>
2022-09-09 02:58:25 -0300

    :work: proibidas ok

--- src/main/java/br/compneusgppremium/api/controller/TipoClassificacaoController.java
+++ src/main/java/br/compneusgppremium/api/controller/TipoClassificacaoController.java
@@ -23,39 +23,39 @@ public class TipoClassificacaoController {
         return values;
     }
 
-//    @GetMapping(path = "/api/tipoclassificacao/{id}")
-//    public ResponseEntity consultar(@PathVariable("id") Integer id) {
-//        return repository.findById(id)
-//                .map(record -> ResponseEntity.ok().body(record))
-//                .orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @PostMapping(path = "/api/tipoclassificacao")
-//    public Object salvar(@RequestBody TipoClassificacaoModel value) {
-//        try {
-//            return repository.save(value);
-//        } catch (Exception ex) {
-//            return ex;
-//        }
-//    }
-//
-//    @PutMapping(path = "/api/tipoclassificacao/{id}")
-//    public ResponseEntity atualizar(@PathVariable("id") Integer id, @RequestBody TipoClassificacaoModel value) {
-//        return repository.findById(id)
-//                .map(record -> {
-//                    record.setDescricao(value.getDescricao());
-//                    TipoClassificacaoModel updated = repository.save(record);
-//                    return ResponseEntity.ok().body(updated);
-//                }).orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @DeleteMapping(path = "/api/tipoclassificacao/{id}")
-//    public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
-//        return repository.findById(id)
-//                .map(record -> {
-//                    repository.deleteById(id);
-//                    return ResponseEntity.ok().build();
-//                }).orElse(ResponseEntity.notFound().build());
-//    }
+    @GetMapping(path = "/api/tipoclassificacao/{id}")
+    public ResponseEntity consultar(@PathVariable("id") Integer id) {
+        return repository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(path = "/api/tipoclassificacao")
+    public Object salvar(@RequestBody TipoClassificacaoModel value) {
+        try {
+            return repository.save(value);
+        } catch (Exception ex) {
+            return ex;
+        }
+    }
+
+    @PutMapping(path = "/api/tipoclassificacao/{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Integer id, @RequestBody TipoClassificacaoModel value) {
+        return repository.findById(id)
+                .map(record -> {
+                    record.setDescricao(value.getDescricao());
+                    TipoClassificacaoModel updated = repository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/api/tipoclassificacao/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
 
 }
