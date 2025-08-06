0b2fb0b2375bf1b94d37e90fdcfab60ef636abda
my <sandysantosjo316@gmail.com>
2025-08-05 23:38:35 -0300

    :sparkles: MODELO cobertura 1.4.0

--- scripts/scriptimplantacao_cobertura.sql
+++ scripts/scriptimplantacao_cobertura.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2025-08-05 22:53
+-- Generated: 2025-08-05 23:37
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -9,15 +9,22 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-ALTER TABLE `gppremium`.`cobertura`
-DROP FOREIGN KEY `fk_cobertura_producao1`;
-
-ALTER TABLE `gppremium`.`cobertura`
-ADD CONSTRAINT `fk_cobertura_producao1`
-  FOREIGN KEY (`producao_id`)
-  REFERENCES `gppremium`.`producao` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+CREATE TABLE IF NOT EXISTS `gppremium`.`cobertura` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `fotos` JSON NULL DEFAULT NULL,
+  `producao_id` INT(11) NOT NULL,
+  `dt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dt_update` DATETIME NULL DEFAULT NULL,
+  `dt_delete` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_cobertura_producao1_idx` (`producao_id` ASC) VISIBLE,
+  CONSTRAINT `fk_cobertura_producao1`
+    FOREIGN KEY (`producao_id`)
+    REFERENCES `gppremium`.`producao` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 18
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
